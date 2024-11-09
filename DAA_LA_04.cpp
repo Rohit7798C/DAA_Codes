f85d825692d8dce8c0478340aaa96c0ecbdef36c
Rohit Rajkumar Chauhan <114377886+Rohit7798C@users.noreply.github.com>
2024-11-09 11:30:27 +0530

    Update DAA_LA_04.cpp

--- DAA_LA_04.cpp
+++ DAA_LA_04.cpp
@@ -1,34 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <limits>
+
 using namespace std;
 
-pair<int, vector<int>> knapsack_01(int n, vector<int>& values, vector<int>& weights, int W) {
-    vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+struct Item {
+    int weight;
+    int value;
+};
 
-    for (int i = 0; i <= n; i++) {
-        for (int w = 0; w <= W; w++) {
-            if (i == 0 || w == 0) {
-                dp[i][w] = 0;
-            } else if (weights[i - 1] <= w) {
-                dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1]);
-            } else {
-                dp[i][w] = dp[i - 1][w];
-            }
-        }
+int knapsack_bb(vector<Item>& items, int W, int n) {
+    // Base case
+    if (n == 0 || W == 0) {
+        return 0;
     }
 
-    vector<int> selected_items;
-    int i = n, w = W;
-
-    while (i > 0 && w > 0) {
-        if (dp[i][w] != dp[i - 1][w]) {
-            selected_items.push_back(i - 1);
-            w -= weights[i - 1];
-        }
-        i--;
+    // If the weight of the current item is more than the knapsack capacity,
+    // then this item cannot be included in the optimal solution
+    if (items[n - 1].weight > W) {
+        return knapsack_bb(items, W, n - 1);
     }
 
-    return {dp[n][W], selected_items};
+    // Recursively compute the maximum value by including or excluding the current item
+    int include = items[n - 1].value + knapsack_bb(items, W - items[n - 1].weight, n - 1);
+    int exclude = knapsack_bb(items, W, n - 1);
+
+    return max(include, exclude);
 }
 
 int main() {
@@ -36,26 +33,18 @@ int main() {
     cout << "Enter the number of items: ";
     cin >> n;
 
-    vector<int> values(n), weights(n);
+    vector<Item> items(n);
     cout << "Enter the maximum weight of the knapsack: ";
     cin >> W;
 
     cout << "Enter the values and weights for each item (value weight):\n";
     for (int i = 0; i < n; i++) {
         cout << "Item " << i + 1 << ": ";
-        cin >> values[i] >> weights[i];
+        cin >> items[i].value >> items[i].weight;
     }
 
-    auto result = knapsack_01(n, values, weights, W);
-    int max_value = result.first;
-    vector<int> selected_items = result.second;
-
+    int max_value = knapsack_bb(items, W, n);
     cout << "Maximum value: " << max_value << endl;
-    cout << "Selected items: ";
-    for (int item : selected_items) {
-        cout << item << " "; // Displaying the index of selected items
-    }
-    cout << endl;
 
     return 0;
 }
