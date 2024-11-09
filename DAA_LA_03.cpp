681b61ec699ea73944fda365464a9eaf5c279feb
Rohit Rajkumar Chauhan <114377886+Rohit7798C@users.noreply.github.com>
2024-11-09 11:29:42 +0530

    Update DAA_LA_03.cpp

--- DAA_LA_03.cpp
+++ DAA_LA_03.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <algorithm> // Include this for 'sort'
+#include <algorithm>
 #include <chrono>
 #include <iomanip>
-#include <vector>    // Include for std::vector
 
 using namespace std;
-
+// A struct in C++ is a user-defined data type that groups variables of different data types under a single name.
 struct Item {
     int value;
     int weight;
@@ -16,51 +15,90 @@ public:
     static bool comp(Item a, Item b) {
         double r1 = static_cast<double>(a.value) / static_cast<double>(a.weight);
         double r2 = static_cast<double>(b.value) / static_cast<double>(b.weight);
-        return r1 > r2; // Sort in descending order
+        return r1 > r2;
     }
 
-    double fractionalKnapsack(int W, vector<Item>& arr) {
-        sort(arr.begin(), arr.end(), comp); // Sort items by value-to-weight ratio
+    double fractionalKnapsack(int W, Item arr[], int n) {
+        sort(arr, arr + n, comp);
         int curWeight = 0;
         double finalValue = 0.0;
 
-        for (int i = 0; i < arr.size(); i++) {
+        for (int i = 0; i < n; i++) {
             if (curWeight + arr[i].weight <= W) {
                 curWeight += arr[i].weight;
                 finalValue += arr[i].value;
             } else {
-                int remain = W - curWeight; // Remaining weight capacity
-                finalValue += (arr[i].value / static_cast<double>(arr[i].weight)) * static_cast<double>(remain);
-                break; // Knapsack is full
+                int remain = W - curWeight;
+                finalValue += (arr[i].value / static_cast<double>(arr[i].weight)) *
+                               static_cast<double>(remain);
+                break;
             }
         }
+
         return finalValue;
     }
 };
 
 int main() {
-    int n, weight; // Number of items and maximum weight of knapsack
-    cout << "Enter the number of items: ";
-    cin >> n;
-
-    cout << "Enter the maximum weight of the knapsack: ";
-    cin >> weight;
-
-    vector<Item> arr(n); // Dynamic array of items
-    for (int i = 0; i < n; i++) {
-        cout << "Enter value and weight for item " << i + 1 << ": ";
-        cin >> arr[i].value >> arr[i].weight;
-    }
-
+    int n = 3, weight = 50;
+    Item arr[n] = {{100, 20}, {60, 10}, {120, 30}};
     Solution obj;
 
-    auto start_time = chrono::high_resolution_clock::now(); // Start time
-    double ans = obj.fractionalKnapsack(weight, arr); // Calculate maximum value
-    auto end_time = chrono::high_resolution_clock::now(); // End time
-    auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time); // Calculate duration
+    auto start_time = chrono::high_resolution_clock::now();
+    double ans = obj.fractionalKnapsack(weight, arr, n);
+    auto end_time = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
 
-    cout << "The maximum value is " << fixed << setprecision(2) << ans << endl; // Display result
-    cout << "Elapsed Time: " << duration.count() << " microseconds" << endl; // Display elapsed time
+    cout << "The maximum value is " << fixed << setprecision(2) << ans << endl;
+    cout << "Elapsed Time: " << duration.count() << " microseconds" << endl;
 
     return 0;
 }
+
+// Optimised code is below
+
+// #include <iostream>
+// #include <algorithm>
+// #include <vector>
+
+// using namespace std;
+
+// struct Item {
+//     int value, weight;
+// };
+
+// bool cmp(Item a, Item b) {
+//     double r1 = (double)a.value / a.weight;
+//     double r2 = (double)b.value / b.weight;
+//     return r1 > r2;
+// }
+
+// double fractionalKnapsack(int W, vector<Item> arr) {
+//     sort(arr.begin(), arr.end(), cmp);
+
+//     double finalValue = 0.0;
+//     int curWeight = 0;
+
+//     for (Item item : arr) {
+//         if (curWeight + item.weight <= W) {
+//             curWeight += item.weight;
+//             finalValue += item.value;
+//         } else {
+//             int remain = W - curWeight;
+//             finalValue += item.value * ((double)remain / item.weight);
+//             break;
+//         }
+//     }
+
+//     return finalValue;
+// }
+
+// int main() {
+//     int W = 50;
+//     vector<Item> arr = {{100, 20}, {60, 10}, {120, 30}};
+
+//     double maxValue = fractionalKnapsack(W, arr);
+//     cout << "Maximum value we can obtain is " << maxValue << endl;
+
+//     return 0;
+// }
